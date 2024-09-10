317b9379a4eaf1a3786ed523e077faed9245d79a
AlexandraB-C <abujorcobili@gmail.com>
2024-09-10 09:36:06 +0300

    reset repository structure

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { faCircle } from '@fortawesome/free-solid-svg-icons';
 import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { faCalendarDay } from '@fortawesome/free-solid-svg-icons';
-
+import { faArrowLeft, faClock, faTag, faUser } from '@fortawesome/free-solid-svg-icons';
 
 // Class representing a Place
 class Place {
@@ -44,28 +44,7 @@ class Event {
 }
 
 // Component to display upcoming events
-function UpcomingEvents({ events }) {
-  const lastEvents = events.slice(-2); // Gets the last two events
-
-  return (
-    <div>
-      {lastEvents.map((upcomingEvent, index) => (
-        <div key={index} className='upcoming-event-css'>
-          <h4>Title: {upcomingEvent.titlu}</h4>
-          <p className='upcoming-event-paragraph-css'>
-            Description: {upcomingEvent.descriere}<br />
-            Date: {upcomingEvent.data.toDateString()}<br />
-            Type: {upcomingEvent.tip}<br />
-            Organizer: {upcomingEvent.organizer.nume}<br />
-            Place: {upcomingEvent.place.oras}, {upcomingEvent.place.strada}
-          </p>
-        </div>
-      ))}
-    </div>
-  );
-}
-
-function HalfUpcomingEvents({ events }) {
+function HalfUpcomingEvents({ events, onShowMore }) {
   const lastEvents = events.slice(-2);
   let arrayEventTypes = [];
 
@@ -83,41 +62,35 @@ function HalfUpcomingEvents({ events }) {
         <div key={index}>
           {arrayEventTypes[index] ? (
             <div className='mandatory-events-css'>
-            <div className='date-icon-date-css-1'>
-              <div> <DateIcon/> </div>
-              <div> <p>{event.data.toDateString()}</p> </div>
-            </div>
-
-            <div className='point-time-css-1'>
+              <div className='date-icon-date-css-1'>
+                <div> <DateIcon/> </div>
+                <div> <p>{event.data.toDateString()}</p> </div>
+              </div>
+              <div className='point-time-css-1'>
                 <div className='point-icon-1'> <PointIcon /> </div>
                 <div className='hour-css-1'> <p>{event.ora}</p> </div>
               </div>
-
-            <div className='title-css-1'> <h4>{event.titlu}</h4> </div>
-
-            <div className='descriere-css-1'> <p> {event.descriere} </p> </div>
-
-              <ShowMoreIcon/>
-
+              <div className='title-css-1'> <h4>{event.titlu}</h4> </div>
+              <div className='descriere-css-1'> <p> {event.descriere} </p> </div>
+              <div onClick={() => onShowMore(event)}>
+                <ShowMoreIcon/>
+              </div>
             </div>
           ) : (
             <div className='optional-events-css'>
-            <div className='date-icon-date-css-2'>
-              <div> <DateIcon/> </div>
-              <div> <p>{event.data.toDateString()}</p> </div>
-            </div>
-
-            <div className='point-time-css-2'>
+              <div className='date-icon-date-css-2'>
+                <div> <DateIcon/> </div>
+                <div> <p>{event.data.toDateString()}</p> </div>
+              </div>
+              <div className='point-time-css-2'>
                 <div className='point-icon-2'> <PointIcon /> </div>
                 <div className='hour-css-2'> <p>{event.ora}</p> </div>
               </div>
-
               <div className='title-css-2'> <h4>{event.titlu}</h4> </div>
-
               <div className='descriere-css-2'> <p> {event.descriere} </p> </div>
-
-              <ShowMoreIcon/>
-
+              <div onClick={() => onShowMore(event)}>
+                <ShowMoreIcon/>
+              </div>
             </div>
           )}
         </div>
@@ -127,26 +100,39 @@ function HalfUpcomingEvents({ events }) {
 }
 
 // Component to display filtered events
-function FilteredEvents({ events }) {
+function FilteredEvents({ event, onBack }) {
   return (
-    <div>
-      {events.length > 0 ? (
-        events.map((event, index) => (
-          <div key={index} className='filtered-event-css'>
-            <h4>Title: {event.titlu}</h4>
-            <p className='filtered-event-paragraph-css'>
-              Description: {event.descriere}<br />
-              Date: {event.data.toDateString()}<br />
-              Type: {event.tip}<br />
-              Organizer: {event.organizer.nume}<br />
-              Place: {event.place.oras}, {event.place.strada}
-            </p>
+    <div> 
+      {events.length > 0 ? ( 
+        <div className="filtered-events-container">
+          <button onClick={onBack} className="back-button">
+            <FontAwesomeIcon icon={faArrowLeft} /> Back
+          </button>
+          <div className='filtered-event-css'>
+            <h4>{event.titlu}</h4>
+            <p>{event.descriere}</p>
+            <div className="event-detail">
+              <FontAwesomeIcon icon={faCalendarDay} className="event-detail-icon" />
+              <span className="event-detail-text">{event.data.toDateString()}</span>
+            </div>
+            <div className="event-detail">
+              <FontAwesomeIcon icon={faClock} className="event-detail-icon" />
+              <span className="event-detail-text">{event.ora}</span>
+            </div>
+            <div className="event-detail">
+              <FontAwesomeIcon icon={faTag} className="event-detail-icon" />
+              <span className="event-detail-text">{event.tip}</span>
+            </div>
+            <div className="event-detail">
+              <FontAwesomeIcon icon={faUser} className="event-detail-icon" />
+              <span className="event-detail-text">{event.organizer.nume}</span>
+            </div>
+            <div className="event-detail">
+              <FontAwesomeIcon icon={faMapMarkerAlt} className="event-detail-icon" />
+              <span className="event-detail-text">{event.place.oras}, {event.place.strada}</span>
+            </div>
           </div>
-        ))
-      ) : (
-        <p>No events found.</p>
-      )}
-    </div>
+        </div>
   );
 }
 
@@ -224,10 +210,7 @@ function App() {
   const [calendarEvents, setCalendarEvents] = useState({});
   const [filteredEvents, setFilteredEvents] = useState([]);
   const [searchPerformed, setSearchPerformed] = useState(false);
-  const [showCalendar, setShowCalendar] = useState(true); 
-  const [showUpcomingEvents, setUpcomingEvents] = useState(true);
-  const [showEventFromCalendar, setEventFromCalendar] = useState(null);
-  const [showHalfUpcomingEvents, setHalfUpcomingEvents] = useState(true);
+  const [selectedEvent, setSelectedEvent] = useState(null);
 
   useEffect(() => {
     const eventsByDate = events.reduce((accumulation, event) => {
@@ -242,7 +225,6 @@ function App() {
     setCalendarEvents(eventsByDate);
   }, [events]);
 
-  // Function to handle the search click
   const handleSearchClick = () => {
     const searchQuery = inputValue.toLowerCase();
     const filtered = events.filter(event =>
@@ -250,41 +232,47 @@ function App() {
       event.descriere.toLowerCase().includes(searchQuery) ||
       event.organizer.nume.toLowerCase().includes(searchQuery)
     );
-    setFilteredEvents(filtered); // Update filtered events
-    setSearchPerformed(true); // Indicate that search has been performed
-    setShowCalendar(false); // Hide calendar after search
+    setFilteredEvents(filtered);
+    setSearchPerformed(true);
+    setSelectedEvent(null);
   };
 
   const handleEventCalendarClick = (event) => {
-    setUpcomingEvents(false);
-    setEventFromCalendar(event);
-  }
+    setSelectedEvent(event);
+  };
 
-  // Function to render event details on the calendar
   const tileContent = ({ date, view }) => {
     const dateKey = date.toDateString();
     const eventsForDate = calendarEvents[dateKey];
-
+    const currentDate = new Date();
+    const isCurrentMonth = date.getMonth() === currentDate.getMonth() && date.getFullYear() === currentDate.getFullYear();
+  
     return view === 'month' && eventsForDate ? (
-      <div>
+      <div className={`event-on-calendar-button-css ${!isCurrentMonth ? 'faded-date' : ''}`}>
         {eventsForDate.map((event, index) => (
-          <div key={index} className='event-on-calendar-button-css' onClick={() => handleEventCalendarClick(event)}>
-            <div className='event-css'>
+          <div key={index} onClick={() => handleEventCalendarClick(event)}>
+            <div className={`event-css ${event.tip === 'Obligatoriu' ? 'mandatory-event' : 'optional-event'}`}>
               <p>{event.titlu}</p>
               <p>{event.ora}</p>
             </div>
           </div>
-        
-        
         ))}
       </div>
     ) : null;
   };
 
-  // Function to handle the calendar button click
   const handleCalendarClick = () => {
-    setShowCalendar(true); // Show the calendar
-    setSearchPerformed(false); // Reset search state
+    setSearchPerformed(false);
+    setSelectedEvent(null);
+  };
+
+  const handleBackClick = () => {
+    setSearchPerformed(false);
+    setSelectedEvent(null);
+  };
+
+  const handleShowMore = (event) => {
+    setSelectedEvent(event);
   };
 
   return (
@@ -292,16 +280,24 @@ function App() {
       <div className="gradient-background">
         <div className="calendar-and-upc-events-css">
           <div className="upcoming-events-or-events-from-calendar">
-            {showUpcomingEvents ? (
+            {!searchPerformed && !selectedEvent && (
               <div className="upcoming-events-section">
                 <h2 className='upcoming-events-text-css'>Upcoming Events</h2>
-                <HalfUpcomingEvents events={events} />
+                <HalfUpcomingEvents events={events} onShowMore={handleShowMore} />
               </div>
-            ) : showEventFromCalendar ? (
-              <div className='event-from-calendar'>
-                <FilteredEvents events={[showEventFromCalendar]} />
-              </div>
-            ) : null}
+            )}
+            {searchPerformed && (
+              <FilteredEvents 
+                event={filteredEvents[0]} 
+                onBack={handleBackClick}
+              />
+            )}
+            {selectedEvent && (
+              <FilteredEvents 
+                event={selectedEvent}
+                onBack={handleBackClick}
+              />
+            )}
           </div>
 
           <div className="calendar-section">
@@ -323,12 +319,8 @@ function App() {
               </div>
             </div>
             <hr className='line-css' />
-            <div className='calendar-or-events'>
-              {searchPerformed ? (
-                <FilteredEvents events={filteredEvents} />
-              ) : showCalendar ? (
-                <div className='calendar-css'><Calendar tileContent={tileContent} /></div>
-              ) : null}
+            <div className='calendar-css'>
+              <Calendar tileContent={tileContent} />
             </div>
           </div>
         </div>
